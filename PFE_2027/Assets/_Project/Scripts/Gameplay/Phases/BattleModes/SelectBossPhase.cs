14e73a2dde463c4152b94c55f424b8d8bb2425cd
OptimistGD <optimist.professionel@gmail.com>
2026-09-11 16:33:34 +0200

    implémentation des phases
    aled

---
+++ PFE_2027/Assets/_Project/Scripts/Gameplay/Phases/BattleModes/SelectBossPhase.cs
@@ -0,0 +1,23 @@
+﻿using System.Threading;
+using Helteix.Tools.Phases;
+using PFE.Core;
+using UnityEngine;
+
+namespace PFE.Gameplay.Scripts.Phases
+{
+    public class SelectBossPhase :  Phase<BossData>
+    {
+        private readonly BossData previousBoss;
+
+        public SelectBossPhase(BossData previousBoss)
+        {
+            this.previousBoss = previousBoss;
+        }
+
+        protected override async Awaitable<BossData> Execute(CancellationToken token)
+        {
+            await Awaitable.MainThreadAsync();
+            return null;
+        }
+    }
+}
